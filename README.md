8663ec96a9d348cbb897b67e71f69b65639d64da
anki <andreas.kist@uk-erlangen.de>
2019-06-17 12:17:57 +0200

    Added local changes, history, floodfill, comments

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # annotator
-Allows to label data using painting or flood filling
+Allows to label data using painting or flood filling.
+It opens tiff stacks and videos (as supported by imageio ```mimread```), and
+can operate on single frames.
+
+This version operates only on foreground/background, the next version
+supports also multi-labels (across multiple time steps / z).
+
+To try out the ```annotator``` tool, simple close the **Open File** dialog,
+to get some dummy data.
+
+# How it works
+
+1) Open a video
+2) The brush is by default magenta, the foreground green, you can change these colors in the settings, 
+and you are able to save and restore old settings.
+3) Draw with left mouse click, you can paint a larger surface by keeping the left mouse button pressed.
+4) Remove area with right mouse click
+5) Use the mouse wheel to zoom in/out 
+6) Adjust contrast/brightness by adjusting the levels on the right hand side. These settings are kept for the entire video.
+7) To move the scene, keep the ```Shift``` key pressed.
+8) For flood fill mode, keep ```Ctrl``` pressed, and click on the desired seed pixel.
+
+# Saving
+
+Everything is stored as HDF5 file, the dimensions are (z/time, x, y), dtype is boolean.
+
+# Shortcuts
+
+These shortcuts make your life much easier:
+
+- ```X``` to remove the mask
+- ```Ctrl+Z``` go back in history
+- ```Ctrl+S``` save mask/segmentation
+- ```C``` copy mask from previous frame
+- ```Q``` toggle mask on/off
+- ```W```, ```A```, ```S```, ```D``` to change frame forward/backward
+- ```M``` change brush from circle to block
+- ```2``` make brush smaller (as small as 1 px)
+- ```8``` make brush bigger 
+
+
--- regionannotator.py
+++ regionannotator.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QSlider, QLabel, QFileDialog, QColorDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QSlider, QLabel, QFileDialog, QColorDialog, QMessageBox
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt, pyqtSignal
 import pyqtgraph as pg
@@ -8,6 +8,8 @@ import deepdish as dd
 import os
 from skimage.draw import circle
 import json
+from glob import glob
+from floodfill import floodfill
 
 
 class customImageItem(pg.ImageItem):
@@ -15,6 +17,7 @@ class customImageItem(pg.ImageItem):
         super().__init__(*args, **kwargs)
         self.clicked = False
         self.mode = 'add'
+        self.save_history = False
 
     def mouseDragEvent(self, e):
         modifiers = QApplication.keyboardModifiers()
@@ -42,13 +45,12 @@ class customImageItem(pg.ImageItem):
             elif e.isFinish():
                 self.clicked = False
 
-    #def mouseClickEvent(self, ev):
-    #    self.clicked = True
-    #    super().mouseClickEvent(ev)
+            else:
+                self.save_history = False
 
 
 class Annotator(pg.ImageView):
-    mySignal = pyqtSignal(int)
+    keyPressSignal = pyqtSignal(int)
 
     def __init__(self, im, mask=None, parent=None):
         # Set Widget as parent to show ImageView in Widget
@@ -57,6 +59,9 @@ class Annotator(pg.ImageView):
         # Set 2D image
         self.setImage(im)
         self.shape = im.shape
+
+        self.history = []
+        self.saved = True
 
         # Colors
         self.colorCursor = (255, 0, 100, 255)  # magenta
@@ -82,6 +87,7 @@ class Annotator(pg.ImageView):
         # Current mask
         self.mask = np.zeros(self.shape + (4,), dtype=np.uint8)
 
+        # If there's already a mask
         if mask is not None:
             self.mask[mask] = self.colorMask
 
@@ -92,7 +98,7 @@ class Annotator(pg.ImageView):
 
         # Radius of block or circle
         self.radius = 6
-        self.mode = 'block'
+        self.mode = 'circle'
         self.showMask = 1
 
         # Add mask and cursor as overlay images,
@@ -106,11 +112,12 @@ class Annotator(pg.ImageView):
         # self.getView().setLeftButtonAction('rect') # Zoom via rectangle
 
     def keyPressEvent(self, ev):
-        self.mySignal.emit(ev.key())
+        self.keyPressSignal.emit(ev.key())
+        modifiers = QApplication.keyboardModifiers()
 
         # Increase radius
         if ev.key() == Qt.Key_8:
-            if self.radius <= 10:
+            if self.radius <= 14:
                 self.radius += 2 if self.mode == 'block' else 1
 
         # Decrease radius
@@ -118,6 +125,10 @@ class Annotator(pg.ImageView):
             if self.radius >= 2:
                 self.radius -= 2 if self.mode == 'block' else 1
 
+            elif self.radius == 1:
+                self.radius = 0
+
+        # Toggle mask visibility
         elif ev.key() == Qt.Key_Q:
             if self.showMask:
                 self.maskItem.setImage(np.zeros_like(self.mask))
@@ -130,6 +141,15 @@ class Annotator(pg.ImageView):
         elif ev.key() == Qt.Key_M:
             self.mode = 'circle' if self.mode == 'block' else 'block'
 
+        elif ev.key() == Qt.Key_Z and modifiers == Qt.ControlModifier:
+            if len(self.history):
+                old_mask = self.history.pop()
+                self.mask[:, :] = old_mask
+                self.maskItem.setImage(self.mask)
+
+        elif ev.key() == Qt.Key_X:
+            self.mask[:, :] = False
+
         self.paint()
 
     def mousePressEvent(self, e):
@@ -138,10 +158,12 @@ class Annotator(pg.ImageView):
         if modifiers != Qt.ShiftModifier:
             if e.button() == Qt.LeftButton:
                 self.maskItem.mode = 'add'
+                self.maskItem.save_history = True
                 self.paint(True)
 
             if e.button() == Qt.RightButton:
                 self.maskItem.mode = 'remove'
+                self.maskItem.save_history = True
                 self.paint(True)
 
     def paint(self, forcePaint=False):
@@ -155,39 +177,62 @@ class Annotator(pg.ImageView):
         xy = self.getImageItem().mapFromScene(self.xy)
         radius = self.radius
 
+        if xy.x() < 0 or xy.x() >= self.shape[0] or xy.y() < 0 or xy.y() >= self.shape[1]:
+            return
+
         # Show current cursor position and painting preview
         self.currentCursor = np.zeros_like(self.currentCursor)
+        cursorMask = np.zeros(self.shape, dtype=np.bool)
 
-        if self.mode == 'block':
-            self.currentCursor[int(xy.x() - radius // 2):int(xy.x() + radius // 2 + 1),
-                     int(xy.y() - radius // 2):int(xy.y() + radius // 2) + 1] = self.colorCursor
+        # Different mask modes
+        # Single pixel
+        if self.radius == 0:
+            cursorMask[int(xy.x()), int(xy.y())] = True
 
+        # Square
+        elif self.mode == 'block':
+            cursorMask[int(xy.x() - radius // 2):int(xy.x() + radius // 2 + 1),
+                       int(xy.y() - radius // 2):int(xy.y() + radius // 2) + 1] = True
+
+        # Circle
         else:
             rr, cc = circle(xy.x(), xy.y(), radius, self.shape)
-            self.currentCursor[rr, cc] = self.colorCursor
+            cursorMask[rr, cc] = True
+
+        # Show the cursorMask colored
+        self.currentCursor[cursorMask] = self.colorCursor
 
         # if mouse is clicked without SHIFT
         if self.maskItem.clicked or forcePaint:
             # Depending on mode,
             #  add or remove pixels from mask
             val = self.colorMask if self.maskItem.mode == 'add' else self.colorBlack
+            modifiers = QApplication.keyboardModifiers()
+
+            if self.maskItem.save_history:
+                self.history.append(self.mask.copy())
 
             # Assign value
-            if self.mode == 'block':
-                self.mask[int(xy.x() - radius // 2):int(xy.x() + radius // 2 + 1),
-                          int(xy.y() - radius // 2):int(xy.y() + radius // 2) + 1] = val
+            # Floodfill using current xy position as seed pixel
+            if modifiers == Qt.ControlModifier:
+                f = floodfill(self.getImageItem().image,
+                              (int(xy.x()),
+                               int(xy.y())))
 
+                self.mask[f == 1] = val
+
+            # Otherwise use the current cursor mask
             else:
-                rr, cc = circle(xy.x(), xy.y(), radius, self.shape)
-                self.mask[rr, cc] = val
-
-            # Update image
-            self.maskItem.setImage(self.mask)
+                self.mask[cursorMask] = val
 
             self.showMask = True
+            self.saved = False
 
         # Update cursor image
         self.currentCursorItem.setImage(self.currentCursor)
+        # Update mask image
+        if self.showMask:
+            self.maskItem.setImage(self.mask)
 
         # Debugging - found out that one needs to accept the drag event
         # self.setWindowTitle('{}'.format(self.maskItem.clicked))
@@ -202,8 +247,9 @@ class Annotator(pg.ImageView):
     def getMask(self):
         return self.mask.sum(2) > 0
 
-    def setZ(self, im, mask=None):
+    def setZ(self, im, mask=None, levels=None):
         self.setImage(im)
+        self.history = []
         self.shape = im.shape
 
         self.mask = np.zeros(self.shape + (4,), dtype=np.uint8)
@@ -212,7 +258,6 @@ class Annotator(pg.ImageView):
             self.mask[mask] = self.colorMask
 
         self.maskItem.setImage(self.mask)
-
         self.paint()
 
     def setColor(self, colorCursor=None, colorMask=None, colorOthers=None, colorBlack=None):
@@ -244,6 +289,7 @@ class Stack(QWidget):
             self.mask = mask
 
         self.curId = 0
+        self.listActive = False
 
         self.w = Annotator(self.stack[self.curId],
                            self.mask[self.curId],
@@ -262,7 +308,7 @@ class Stack(QWidget):
         self.z.setValue(0)
         self.z.setSingleStep(1)
         self.z.valueChanged.connect(self.changeZ)
-        self.w.mySignal.connect(self.keyPress)
+        self.w.keyPressSignal.connect(self.keyPress)
 
         self.l.addWidget(QLabel("z position"), 1, 0)
         self.l.addWidget(self.z, 1, 1)
@@ -270,19 +316,28 @@ class Stack(QWidget):
         self.setLayout(self.l)
 
     def changeZ(self):
+        # Save current mask
         self.mask[self.curId] = self.w.getMask()
 
+        # Save current view state (zoom, position, ...)
         viewBoxState = self.w.getView().getState()
+        # Save current levels
+        levels = self.w.getImageItem().levels
 
+        # New image position
         self.curId = self.z.value()
 
         # Boundary check
         self.curId = max(self.curId, 0)
-        self.curId = min(self.curId, self.stack.shape[-1])
+        self.curId = min(self.curId, self.stack.shape[0])
 
-        self.w.setZ(self.stack[self.curId], self.mask[self.curId])
+        # Set the new image
+        im = self.stack[self.curId]
+
+        self.w.setZ(im, self.mask[self.curId], levels)
 
         self.w.getView().setState(viewBoxState)
+        self.w.getImageItem().setLevels(levels)
 
     def keyPress(self, key):
         modifiers = QApplication.keyboardModifiers()
@@ -292,13 +347,18 @@ class Stack(QWidget):
             self.z.setValue(self.curId+1)
 
         elif key == Qt.Key_A or (key == Qt.Key_S and modifiers != Qt.ControlModifier):
-            self.z.setValue(self.curId-1)
+            self.z.setValue(self.curId -1)
 
+        # Copy mask from previous (-1) mask
         elif key == Qt.Key_C:
             if self.mask[self.curId].sum() == 0 and self.curId > 0:
+                # Replace mask
                 self.mask[self.curId] = self.mask[self.curId-1]
+                # Get the state (i.e. position, zoom, ...)
                 viewBoxState = self.w.getView().getState()
+                # Set new mask
                 self.w.setZ(self.stack[self.curId], self.mask[self.curId])
+                # Set view again
                 self.w.getView().setState(viewBoxState)
 
     def getMasks(self):
@@ -309,7 +369,7 @@ class Stack(QWidget):
         modifiers = QApplication.keyboardModifiers()
 
         if e.key() == Qt.Key_S and modifiers == Qt.ControlModifier:
-            self.w.mySignal.emit(e.key())
+            self.w.keyPressSignal.emit(e.key())
 
 class Main(QMainWindow):
     def __init__(self):
@@ -330,18 +390,18 @@ class Main(QMainWindow):
         self.settings.addAction("Load settings", self.loadSettings)
 
         self.fn = None
+        self.list = None
+        self.d = None
+        self.stack = None
 
-        #s = np.random.random_integers(0, 255, 100*100*100).reshape(100, 100, 100).astype(np.uint8)
-
-        #self.stack = Stack(s)
-        #self.setCentralWidget(self.stack)
-
-       # self.stack.z.valueChanged.connect(self.updateStatus)
         self.setGeometry(300, 300, 800, 600)
 
+    def setEqualize(self):
+        if self.stack:
+            self.stack.equalize = self.equalize.isChecked()
+
     def saveSettings(self):
-        settings_fn = QFileDialog.getSaveFileName(directory=r"C:\Users\kistas\PycharmProjects\RegionAnnotator",
-                                                  filter="*.settings")[0]
+        settings_fn = QFileDialog.getSaveFileName(filter="*.settings")[0]
 
         if settings_fn:
             print(settings_fn)
@@ -355,17 +415,19 @@ class Main(QMainWindow):
             self.settings_fn = settings_fn
 
     def loadSettings(self, settings_fn=None):
-
         if settings_fn is None:
-            settings_fn = QFileDialog.getOpenFileName(directory=r"C:\Users\kistas\PycharmProjects\RegionAnnotator",
-                                                      filter="*.settings")[0]
+            settings_fn = QFileDialog.getOpenFileName(filter="*.settings")[0]
 
-        with open(settings_fn, 'r') as fp:
-            settings = json.load(fp)
+        if os.path.isfile(settings_fn):
+            with open(settings_fn, 'r') as fp:
+                settings = json.load(fp)
 
-        self.stack.w.setColor(colorCursor=settings['colorCursor'],
-                              colorMask=settings['colorMask'])
+            self.stack.w.setColor(colorCursor=settings['colorCursor'],
+                                  colorMask=settings['colorMask'])
 
+            self.stack.changeZ()
+
+            self.settings_fn = settings_fn
 
     def updateStatus(self):
         self.status.showMessage('z: {} x: {} y: {}'.format(self.stack.z.value(), self.stack.w.shape[0],  self.stack.w.shape[1]))
@@ -382,13 +444,31 @@ class Main(QMainWindow):
     def setCursorColor(self):
         self.stack.w.setColor(colorCursor=self.getColor(self.stack.w.colorMask))
 
-    def open(self):
-        file = QFileDialog.getOpenFileName(directory=r"W:\phoniatrie\nas-pp1\Backup_NAS_alt\Deep Learning\Daten\Stepp\HSV Data")[0]
+    def open(self, file=None, file_avi=None):
+        # A video is loaded
+        if self.stack is not None:
+            # And it was changed
+            if not self.stack.w.saved:
+                # Ask for saving
+                save_it = QMessageBox.question(self,
+                                                 "Save?",
+                                                 "You haven't saved this file. \nDo you want to save it now?",
+                                                 QMessageBox.Yes | QMessageBox.No)
+
+                if save_it == QMessageBox.Yes:
+                    self.save()
+
+        if file is None:
+            file = QFileDialog.getOpenFileName(directory=self.d)[0]
+
         self.status.showMessage(file)
 
         if file:
             self.fn = file
-            self.fn_mask = ".".join(file.split(".")[:-1])+".mask"
+            self.setWindowTitle(self.fn)
+            self.fn_mask = ".".join(file.split(".")[:-1]) + ".mask"
+
+            self.d = os.path.dirname(self.fn)
 
             s = io.mimread(file, memtest=False)
             s = np.array(s, dtype=s[0].dtype).transpose(0, 2, 1, 3)
@@ -396,23 +476,36 @@ class Main(QMainWindow):
 
             if os.path.isfile(self.fn_mask):
                 mask = dd.io.load(self.fn_mask, "/mask")
-                print("Mask shape: ", mask.shape)
+                print("Mask shape:  ", mask.shape)
             else:
                 mask = None
 
-            self.stack = Stack(s[..., 0], mask)
+            self.stack = Stack(s[..., 0] if len(s.shape) == 4 else s, mask)
             self.setCentralWidget(self.stack)
             self.stack.z.valueChanged.connect(self.updateStatus)
-            self.stack.w.mySignal.connect(self.savekeyboard)
+            self.stack.w.keyPressSignal.connect(self.savekeyboard)
 
         # Debug mode
         else:
-            s = np.random.random_integers(0, 255, 100 * 100 * 20).reshape(20, 100, 100).astype(np.uint8)
+            from skimage.filters import gaussian
+            from skimage.draw import ellipse
+
+            s = np.random.random_integers(173, 255, 100 * 100 * 20).reshape(20, 100, 100).astype(np.uint8)
+
+            for i in range(s.shape[0]):
+                rr, cc = ellipse(s.shape[1]//2, 
+                                 s.shape[2]//2, 
+                                 17 * np.sin(0.2 * i) + 5, 
+                                 40, 
+                                 s.shape[1:])
+
+                s[i][rr, cc] = 125
+                s[i] = gaussian(s[i], 2.5, preserve_range=True)
 
             self.stack = Stack(s)
             self.setCentralWidget(self.stack)
             self.stack.z.valueChanged.connect(self.updateStatus)
-            self.stack.w.mySignal.connect(self.savekeyboard)
+            self.stack.w.keyPressSignal.connect(self.savekeyboard)
 
         self.settings.setEnabled(True)
 
@@ -421,8 +514,20 @@ class Main(QMainWindow):
 
     def save(self):
         if self.fn:
+            self.stack.w.saved = True
+
+            if "ö" in self.fn or "ü" in self.fn or "ö" in self.fn:
+                QMessageBox.information(self, "Umlaut found",
+                                     "You cannot save Umlaut with deepdish... \nI try to avoid it, but this tool will \nnot find the mask file automatically. \nPlease consider renaming it!",
+                                     QMessageBox.Ok)
+
+                print("before: ", self.fn_mask)
+                self.fn_mask = self.fn_mask.replace("ö","oe").replace("ü","ue").replace("ä","ae")
+                print("after:  ", self.fn_mask)
+
             dd.io.save(self.fn_mask, {'mask': self.stack.getMasks()}, compression='blosc')
             print('saving done.')
+
             self.status.showMessage("Masks saved as {} ...".format(self.fn_mask), 1000)
 
     def savekeyboard(self, key):
@@ -439,3 +544,4 @@ if __name__ == '__main__':
     m.show()
 
     sys.exit(app.exec_())
+
